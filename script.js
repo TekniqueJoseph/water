1a325cb386000a51239dcb4404647bc6fe60c75e
TekniqueJoseph <joejacksun@gmail.com>
2023-03-21 22:12:19 -0400

    practice files

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("hey");
+let btn=document.querySelector('#btn');
+btn.addEventListener('click', function () {
+    let click=document.createElement('h3')
+    let letters=document.createTextNode('The Tribe Must Consolidate');
+    click.appendChild(letters);
+    click.classList.add('animate__animated', 'animate__rubberBand');
+    document.querySelector('#pop').appendChild(click)
+}, { once: true })
